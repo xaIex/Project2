1af635d6da5bb808fefe3afb4b29bae0a9db22dd
xcast <xcastillo2001@gmail.com>
2024-04-25 17:08:12 -0700

    sixth commit

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -8,14 +8,13 @@ const {
     renderCart,
     clearCart,
     productCartAdded
-
 } = require('../controllers/cartController');
 
 router.post('/addProduct', addToCart);
 router.post('/removeProduct', removeFromCart);
 router.post('/updateProduct', updateCartQuantity);
 router.get('/cartContents', renderCart);
-router.post('/clearCartContents', clearCart)
+router.post('/clearCartContents', clearCart);
 router.get('/productAddedPage', productCartAdded);
 
 
